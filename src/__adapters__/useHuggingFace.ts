5c9aa8c4f2a90a7908f4068b11ab07e07c896c96
Elliott Lépine <elliott@brad.ag>
2024-02-26 19:08:26 +0100

    Prompting Hugging Face works, yippeee !!!!!

--- src/__adapters__/useHuggingFace.ts
+++ src/__adapters__/useHuggingFace.ts
@@ -8,17 +8,15 @@ export async function useHuggingFace() {
 	const app = await client(
 		'https://team-lfd-sia-pro-chatbot-g-pdf.hf.space/--replicas/wf94h/',
 		{
-			hf_token: '',
+			hf_token: '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',
 		},
 	)
 
 	return {
 		prompt: async (msg: string) => {
-			const result = await app.predict('/respond', [msg])
+			const result = await app.predict('/respond', [msg, []])
 
-			console.log(result)
-
-			return result
+			return result.data[0][1] as string
 		},
 	}
 }
