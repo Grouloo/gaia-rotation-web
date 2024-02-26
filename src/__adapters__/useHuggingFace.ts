eb1c64272938dcc7457e36ba441ef3ba421e74fc
Elliott Lépine <elliott@brad.ag>
2024-02-26 19:11:59 +0100

    Oops i committed my api key :)

--- src/__adapters__/useHuggingFace.ts
+++ src/__adapters__/useHuggingFace.ts
@@ -8,13 +8,15 @@ export async function useHuggingFace() {
 	const app = await client(
 		'https://team-lfd-sia-pro-chatbot-g-pdf.hf.space/--replicas/wf94h/',
 		{
-			hf_token: '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',
+			hf_token: 'hf_',
 		},
 	)
 
 	return {
 		prompt: async (msg: string) => {
-			const result = await app.predict('/respond', [msg, []])
+			const result = (await app.predict('/respond', [msg, []])) as never as {
+				data: any
+			}
 
 			return result.data[0][1] as string
 		},
