f6e3c8da924b72710cf06ec60834f0c918cd9c52
Elliott Lépine <elliott@brad.ag>
2024-02-27 13:34:15 +0100

    Add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import vercel from "@astrojs/vercel/serverless";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel()
+  adapter: netlify()
 });
