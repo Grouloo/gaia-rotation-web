39cbe5f38a1fcc70cd841aa210d2270646d5a341
Elliott Lépine <elliott@brad.ag>
2024-02-27 14:38:04 +0100

    Fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/serverless";
-import netlify from "@astrojs/netlify";
-import node from "@astrojs/node";
+import { defineConfig } from 'astro/config'
+//import vercel from "@astrojs/vercel/serverless";
+//import netlify from "@astrojs/netlify";
+//import node from "@astrojs/node";
 
-import cloudflare from "@astrojs/cloudflare";
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare()
-});
+	output: 'server',
+	adapter: cloudflare(),
+})
