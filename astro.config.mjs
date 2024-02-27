88659ea993409b59baed41ae87454ac172a6a635
Elliott Lépine <elliott@brad.ag>
2024-02-27 14:36:00 +0100

    Add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
 import netlify from "@astrojs/netlify";
-
 import node from "@astrojs/node";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: cloudflare()
 });
