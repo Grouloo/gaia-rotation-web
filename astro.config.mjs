611a4b184cbdea77f9a42f0d522e19e3bf7d9a9b
Elliott Lépine <elliott@brad.ag>
2024-02-27 13:59:42 +0100

    Add dockerfile + node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
-
 import netlify from "@astrojs/netlify";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: netlify()
+  adapter: node({
+    mode: "standalone"
+  })
 });
