7c3ecc7b3be9f6d11113bf3592645f569659cbc9
Elliott Lépine <elliott@brad.ag>
2024-02-27 10:41:01 +0100

    App<->HuggingFace connection works

--- src/__adapters__/useHuggingFace.ts
+++ src/__adapters__/useHuggingFace.ts
@@ -1,6 +1,8 @@
+import type { FranceDepartments } from '@domain'
 import { client } from '@gradio/client'
 // @ts-expect-error
 import eventsource from 'eventsource'
+import { Ok, type AsyncResult, Err } from 'shulk'
 //@ts-expect-error
 global.EventSource = eventsource
 
@@ -20,5 +22,30 @@ export async function useHuggingFace() {
 
 			return result.data[0][1] as string
 		},
+
+		findMyRotation: async (params: Input): AsyncResult<Error, string> => {
+			try {
+				const result = await app.predict('/find_my_rotation', [
+					params.department,
+					params.farmSize,
+					params.benefitsFromCommonAgriculturalPolicy,
+					{ headers: [], data: [] },
+					{ headers: [], data: [] },
+				])
+				console.log(result)
+				return Ok((result as any).data as string)
+			} catch (e) {
+				console.error(e)
+				return Err(e as Error)
+			}
+		},
 	}
+}
+
+type Input = {
+	department: FranceDepartments
+	farmSize: number
+	benefitsFromCommonAgriculturalPolicy: boolean
+	cultures: string[]
+	yields: Record<string, number>
 }
